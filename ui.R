f35a7cc8a5a1a7738a4c3d5a27b361c7b976bf0c
Teofil <teofiln@gmail.com>
2014-05-26 17:43:20 -0500

    misc

--- ui.R
+++ ui.R
@@ -15,27 +15,37 @@ shinyUI(fluidPage(
 	titlePanel(h3("An interactive diatom phylogeny", style="color:grey")),
 
 	sidebarLayout(#position="right",
-		sidebarPanel( 
-			p("Select two diatoms and click 'Plot tree' to see the sub-tree starting with their most recent common ancestor"),
+		sidebarPanel(
+			p("Select a lineage to explore or choose 'Custom' and select two diatoms to see the sub-tree starting with their most recent common ancestor", style="color:#0066FF"),
 			br(),
-			selectInput("select1", label = h6("Select first taxon"),
+			selectInput("select1", label = h6("Select lineage"),
+						choices = list(
+							"Custom" = "Custom",
+							"Raphid diatoms" = "Raphid diatoms", 
+							"Pennate diatoms" = "Pennate diatoms", 
+#							"Araphid clade 1" = "Araphid clade 1", 
+#							"Araphid clade 2" = "Araphid clade 2", 
+#							"Polars clade 1" = "Polars clade 1",
+#							"Polars clade 2" = "Polars clade 2",
+#							"Polars clade 3" = "Polars clade 3",
+#							"Radials clade 1" = "Radials clade 1",
+#							"Radials clade 2" = "Radials clade 2",
+							"All diatoms" = "All diatoms"), selected = "All diatoms"),
+			br(), 
+			selectInput("select2", label = h6("Select first taxon"),
 						choices = taxList, selected = 1),
-
-			selectInput("select2", label = h6("Select second taxon"),
+			selectInput("select3", label = h6("Select second taxon"),
 						choices = taxList, selected = 16),
-			br(),
-			actionButton("get1","Plot tree"),
-#			br(),
-			actionButton("get2","Restore default")
+			actionButton("get1","Plot custom sub-tree")
 		),
 
 		mainPanel(
 			tabsetPanel(
-			tabPanel("Big tree",
-				plotOutput("bigTree", height="1000px")),
+			tabPanel("Tree",
+				plotOutput("Tree", height="1000px")),
 #				textOutput("Text"))
-			tabPanel("Some clades"),
-			tabPanel("Studies"),
+#			tabPanel("Some clades"),
+#			tabPanel("Studies"),
 			tabPanel("About", htmlOutput("textAbout"))
 			)
 		)					
